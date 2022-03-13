af7d64a885c16f17106f08c954b36817314acee7
AnimeshKC <animesh@ualberta.ca>
2022-03-13 15:32:05 -0600

    Remove TradeAction

--- client/src/scene/ControlsUI.ts
+++ client/src/scene/ControlsUI.ts
@@ -1,29 +1,28 @@
 import { SceneGameArena } from "./SceneGameArena";
 
 export class ControlsUI {
-  constructor(scene: SceneGameArena, keys: Array<string>) {
-    let y = 550;
-    let index = 0;
-    const controlInfo = [
-      "Move Left",
-      "Move Right",
-      "Move Down",
-      "Rotate CCW",
-      "Rotate CW",
-      "Trade Action"
-    ];
+    constructor(scene: SceneGameArena, keys: Array<string>) {
+        let y = 550;
+        let index = 0;
+        const controlInfo = [
+            "Move Left",
+            "Move Right",
+            "Move Down",
+            "Rotate CCW",
+            "Rotate CW",
+        ];
 
-    for (const key of keys) {
-      scene.add.image(50, y, key).setScale(0.2);
+        for (const key of keys) {
+            scene.add.image(50, y, key).setScale(0.2);
 
-      scene.add
-        .text(70, y - 10, controlInfo[index++], {
-          fontSize: `20px`,
-          fontFamily: "VT323",
-        })
-        .setTint(0xffffff);
+            scene.add
+                .text(70, y - 10, controlInfo[index++], {
+                    fontSize: `20px`,
+                    fontFamily: "VT323",
+                })
+                .setTint(0xffffff);
 
-      y += 40;
+            y += 40;
+        }
     }
-  }
 }
