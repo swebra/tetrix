9a75d0c38c28983bbc97e4a3e13665cf2b264ebb
AnimeshKC <animesh@ualberta.ca>
2022-03-16 20:04:48 -0600

    Make shuffle internal to the class

--- client/src/randomBag.ts
+++ client/src/randomBag.ts
@@ -1,15 +1,5 @@
 import { TetrominoType } from "common/TetrominoType";
 
-function fisherShuffle(arr: Array<any>) {
-    let currIdx = arr.length - 1;
-
-    while (currIdx > 0) {
-        const randIdx = Math.floor(Math.random() * currIdx);
-        [arr[currIdx], arr[randIdx]] = [arr[randIdx], arr[currIdx]];
-        currIdx--;
-    }
-    return arr;
-}
 export class RandomBag{
     bagStack: Array<TetrominoType>;
 
@@ -17,10 +7,22 @@ export class RandomBag{
         this.bagStack = [];
 
     }
+    
+    //using the Fisher Yates algorithm
+    private shuffle() {
+    let currIdx = this.bagStack.length - 1;
+
+    while (currIdx > 0) {
+        const randIdx = Math.floor(Math.random() * currIdx);
+        [this.bagStack[currIdx], this.bagStack[randIdx]] = [this.bagStack[randIdx], this.bagStack[currIdx]];
+        currIdx--;
+    }
+    return this.bagStack;
+    }
+    
     private createBag() {
-        const bagList = [TetrominoType.I, TetrominoType.J, TetrominoType.L, TetrominoType.O, TetrominoType.S, TetrominoType.T, TetrominoType.Z];
-        fisherShuffle(bagList);
-        this.bagStack = bagList;
+        this.bagStack = [TetrominoType.I, TetrominoType.J, TetrominoType.L, TetrominoType.O, TetrominoType.S, TetrominoType.T, TetrominoType.Z];
+        this.shuffle();
     }
     public returnNextPiece() {
         if (this.bagStack.length == 0) {
