f8fe5fd7a333160caab9e9fabbb1caf856864f59
AnimeshKC <animesh@ualberta.ca>
2022-04-06 12:14:20 -0600

    Finish Regular Trade Tests

--- server/src/tests/Trade.test.ts
+++ server/src/tests/Trade.test.ts
@@ -1,5 +1,5 @@
 import { Trade } from "../Trade";
-import { SocketServerMock, SocketClientMock } from "socket.io-mock-ts";
+import SocketMock from "socket.io-mock-ts";
 import { textSpanIntersectsWithTextSpan } from "typescript";
 import { TetrominoType } from "../../../common/TetrominoType";
 
@@ -9,10 +9,21 @@ describe("Testing 'Trade'", () => {
     let serverSocket: any;
     let randTrader: Trade;
     let trader: Trade;
+    let client1Tetromino: TetrominoType | null;
+    let client2Tetromino: TetrominoType | null;
     beforeAll(() => {
-        serverSocket = new SocketServerMock();
-        clientSocket1 = new SocketClientMock(serverSocket);
-        clientSocket2 = new SocketClientMock(serverSocket);
+        client1Tetromino = null;
+        client2Tetromino = null;
+        clientSocket1 = new SocketMock();
+        clientSocket2 = new SocketMock();
+        // clientSocket1.on("sendRandomPiece", (tetromino: TetrominoType) => {
+        //     client1Tetromino = tetromino;
+        // });
+        // clientSocket1.on("sendTradePiece", (tetromino: TetrominoType) => {
+        //     client2Tetromino = tetromino;
+        // });
+        // clientSocket2.on("sendRandomPiece", () => {});
+        // clientSocket2.on("sendTradePiece", () => {});
     });
     beforeEach(() => {
         randTrader = new Trade(1);
@@ -24,5 +35,26 @@ describe("Testing 'Trade'", () => {
         expect(trader.tradeActive).toBe(true);
         expect(trader.currentTradeOffer).toBe(TetrominoType.I);
         expect(trader.pairNum).toBe(null);
+    });
+    test("Second Offer for regular trader", () => {
+        trader.currentOfferer = clientSocket1;
+        trader.tradeActive = true;
+        trader.currentTradeOffer = TetrominoType.I;
+        trader.addTrade(clientSocket2, TetrominoType.J);
+        clientSocket1.on("sendTradePiece", (tetromino: TetrominoType) => {
+            expect(tetromino).toEqual(TetrominoType.J);
+        });
+        clientSocket2.on("sendTradePiece", (tetromino: TetrominoType) => {
+            expect(tetromino).toEqual(TetrominoType.I);
+        });
+    });
+    test("Clear trade", () => {
+        trader.currentOfferer = clientSocket1;
+        trader.tradeActive = true;
+        trader.currentTradeOffer = TetrominoType.I;
+        trader.clearTrade();
+        expect(trader.currentOfferer).toBe(null);
+        expect(trader.currentTradeOffer).toBe(null);
+        expect(trader.tradeActive).toBe(false);
     });
 });
