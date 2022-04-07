87c1328277d232189cdfaf0a52efb9e0d4d6eaec
AnimeshKC <animesh@ualberta.ca>
2022-04-07 10:49:41 -0600

    Add expects for addTrade

--- server/src/tests/Trade.test.ts
+++ server/src/tests/Trade.test.ts
@@ -9,11 +9,9 @@ describe("Testing 'Trade'", () => {
     let randTrader: Trade;
     let trader: Trade;
 
-    beforeAll(() => {
+    beforeEach(() => {
         clientSocket1 = new SocketMock();
         clientSocket2 = new SocketMock();
-    });
-    beforeEach(() => {
         randTrader = new Trade(1);
         trader = new Trade();
     });
@@ -25,16 +23,20 @@ describe("Testing 'Trade'", () => {
         expect(trader.pairNum).toBe(null);
     });
     test("[FR22 Player Accept Trade] The second trade will transmit socket events", () => {
-        clientSocket1.on("sendTradePiece", (tetromino: TetrominoType) => {
-            expect(tetromino).toEqual(TetrominoType.J);
-        });
-        clientSocket2.on("sendTradePiece", (tetromino: TetrominoType) => {
-            expect(tetromino).toEqual(TetrominoType.I);
-        });
         trader.currentOfferer = clientSocket1;
         trader.tradeActive = true;
         trader.currentTradeOffer = TetrominoType.I;
+        const client1Spy = jest.spyOn(clientSocket1, "emit");
+        const client2Spy = jest.spyOn(clientSocket2, "emit");
         trader.addTrade(clientSocket2, TetrominoType.J);
+        expect(client1Spy).toHaveBeenCalledWith(
+            "sendTradePiece",
+            TetrominoType.J
+        );
+        expect(client2Spy).toHaveBeenCalledWith(
+            "sendTradePiece",
+            TetrominoType.I
+        );
     });
     test("[FR21 Player Trade] Trade will Clear", () => {
         trader.currentOfferer = clientSocket1;
@@ -52,16 +54,22 @@ describe("Testing 'Trade'", () => {
         expect(randTrader.currentTradeOffer).toBe(TetrominoType.L);
     });
     test("[FR26 Spectator Random Trade] Storing the second Random Trade client information will transmit socket events", () => {
-        clientSocket1.on("sendRandomPiece", (tetromino: TetrominoType) => {
-            expect(tetromino).toEqual(TetrominoType.J);
-        });
-        clientSocket2.on("sendRandomPiece", (tetromino: TetrominoType) => {
-            expect(tetromino).toEqual(TetrominoType.I);
-        });
         randTrader.currentOfferer = clientSocket1;
         randTrader.tradeActive = true;
         randTrader.currentTradeOffer = TetrominoType.I;
+        const client1Spy = jest.spyOn(clientSocket1, "emit");
+        const client2Spy = jest.spyOn(clientSocket2, "emit");
         randTrader.addTrade(clientSocket2, TetrominoType.J);
+        expect(client1Spy).toHaveBeenCalledWith(
+            "sendRandomPiece",
+            TetrominoType.J,
+            1
+        );
+        expect(client2Spy).toHaveBeenCalledWith(
+            "sendRandomPiece",
+            TetrominoType.I,
+            1
+        );
         //randTrader also clears the trades so these checks are needed
         expect(randTrader.currentOfferer).toBe(null);
         expect(randTrader.currentTradeOffer).toBe(null);
