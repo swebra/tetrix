6c637525a0a21bd98618313b5da346263afb524b
AnimeshKC <animesh@ualberta.ca>
2022-04-07 09:41:13 -0600

    Specifying functional requirements of trade

--- server/src/tests/Trade.test.ts
+++ server/src/tests/Trade.test.ts
@@ -12,27 +12,19 @@ describe("Testing 'Trade'", () => {
     beforeAll(() => {
         clientSocket1 = new SocketMock();
         clientSocket2 = new SocketMock();
-        // clientSocket1.on("sendRandomPiece", (tetromino: TetrominoType) => {
-        //     client1Tetromino = tetromino;
-        // });
-        // clientSocket1.on("sendTradePiece", (tetromino: TetrominoType) => {
-        //     client2Tetromino = tetromino;
-        // });
-        // clientSocket2.on("sendRandomPiece", () => {});
-        // clientSocket2.on("sendTradePiece", () => {});
     });
     beforeEach(() => {
         randTrader = new Trade(1);
         trader = new Trade();
     });
-    test("First Offer for regular trader", () => {
+    test("[FR21 Player Trade] The initial trade offer will be stored", () => {
         trader.addTrade(clientSocket1, TetrominoType.I);
         expect(trader.currentOfferer).toBe(clientSocket1);
         expect(trader.tradeActive).toBe(true);
         expect(trader.currentTradeOffer).toBe(TetrominoType.I);
         expect(trader.pairNum).toBe(null);
     });
-    test("Second Offer for regular trader", () => {
+    test("[FR22 Player Accept Trade] The second trade will transmit socket events", () => {
         clientSocket1.on("sendTradePiece", (tetromino: TetrominoType) => {
             expect(tetromino).toEqual(TetrominoType.J);
         });
@@ -44,7 +36,7 @@ describe("Testing 'Trade'", () => {
         trader.currentTradeOffer = TetrominoType.I;
         trader.addTrade(clientSocket2, TetrominoType.J);
     });
-    test("Clear trade", () => {
+    test("[FR21 Player Trade] Trade will Clear", () => {
         trader.currentOfferer = clientSocket1;
         trader.tradeActive = true;
         trader.currentTradeOffer = TetrominoType.I;
@@ -53,13 +45,13 @@ describe("Testing 'Trade'", () => {
         expect(trader.currentTradeOffer).toBe(null);
         expect(trader.tradeActive).toBe(false);
     });
-    test("First offer for Random Trade", () => {
+    test("[FR26 Spectator Random Trade] The first Random Trade client information will be stored", () => {
         randTrader.addTrade(clientSocket1, TetrominoType.L);
         expect(randTrader.currentOfferer).toBe(clientSocket1);
         expect(randTrader.tradeActive).toBe(true);
         expect(randTrader.currentTradeOffer).toBe(TetrominoType.L);
     });
-    test("Second offer for Random Trade", () => {
+    test("[FR26 Spectator Random Trade] Storing the second Random Trade client information will transmit socket events", () => {
         clientSocket1.on("sendRandomPiece", (tetromino: TetrominoType) => {
             expect(tetromino).toEqual(TetrominoType.J);
         });
